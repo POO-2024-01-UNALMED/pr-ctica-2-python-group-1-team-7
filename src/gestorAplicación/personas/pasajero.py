28ec3d8544f2cc6729bdb0c76b76baceeba49e4c
sacardonaf <sacardonaf@unal.edu.co>
2024-08-31 20:15:57 -0500

    Correción nombre de métodos de la clase Pasajero

--- src/gestorAplicación/personas/pasajero.py
+++ src/gestorAplicación/personas/pasajero.py
@@ -1,97 +1,97 @@
 from datetime import date
-from gestorAplicacion.gestion import Empresa, Tiquete, Viaje
-from gestorAplicacion.personas.persona import Persona
+from gestor_aplicacion.gestion import Empresa, Tiquete, Viaje
+from gestor_aplicacion.personas.persona import Persona
 
 
 class Pasajero(Persona):
     pasajeros = []
 
-    def __init__(self, nombre="Sin nombre", idPasajero="0", telefono="0000000000", correo="No tiene"):
-        super().__init__(nombre, idPasajero, telefono, correo)
+    def __init__(self, nombre="Sin nombre", id_pasajero="0", telefono="0000000000", correo="No tiene"):
+        super().__init__(nombre, id_pasajero, telefono, correo)
         self.tiquetes = []
         Pasajero.pasajeros.append(self)
 
     @staticmethod
-    def buscarPasajero(idPasajero):
+    def buscar_pasajero(id_pasajero):
         for pasajero in Pasajero.pasajeros:
-            if pasajero.getId() == idPasajero:
+            if pasajero.get_id() == id_pasajero:
                 return pasajero
         return None
 
     @staticmethod
-    def buscarPasajeroPorNombre(nombre, idPasajero):
+    def buscar_pasajero_por_nombre(nombre, id_pasajero):
         for pasajero in Pasajero.pasajeros:
             if pasajero.nombre == nombre and pasajero.id is not None:
-                if pasajero.id == idPasajero:
+                if pasajero.id == id_pasajero:
                     return pasajero
         return None
 
-    def buscarTiquetes(self, tipoTiquetes):
-        tiquetesValidos = []
-        tiquetesVencidos = []
+    def buscar_tiquetes(self, tipo_tiquetes):
+        tiquetes_validos = []
+        tiquetes_vencidos = []
 
         for tiquete in self.tiquetes:
-            tiquete.setViaje(Empresa.buscarViaje(tiquete.getViaje().getId()))
-            if tipoTiquetes == "validos" and tiquete.getViaje().getFecha() > date.today():
-                tiquetesValidos.append(tiquete)
-            elif tipoTiquetes == "vencidos" and tiquete.getViaje().getFecha() < date.today():
-                tiquetesVencidos.append(tiquete)
+            tiquete.set_viaje(Empresa.buscar_viaje(tiquete.get_viaje().get_id()))
+            if tipo_tiquetes == "validos" and tiquete.get_viaje().get_fecha() > date.today():
+                tiquetes_validos.append(tiquete)
+            elif tipo_tiquetes == "vencidos" and tiquete.get_viaje().get_fecha() < date.today():
+                tiquetes_vencidos.append(tiquete)
 
-        if tipoTiquetes == "validos":
-            return tiquetesValidos
-        elif tipoTiquetes == "vencidos":
-            return tiquetesVencidos
+        if tipo_tiquetes == "validos":
+            return tiquetes_validos
+        elif tipo_tiquetes == "vencidos":
+            return tiquetes_vencidos
         else:
             return None
 
-    def buscarTiquetePorViaje(self, viaje):
+    def buscar_tiquete_por_viaje(self, viaje):
         for tiquete in self.tiquetes:
-            tiquete.setViaje(Empresa.buscarViaje(viaje.getId()))
-            if tiquete.getViaje() == viaje:
+            tiquete.set_viaje(Empresa.buscar_viaje(viaje.get_id()))
+            if tiquete.get_viaje() == viaje:
                 return tiquete
         return None
 
-    def buscarTiquetePorReserva(self, numeroReserva):
+    def buscar_tiquete_por_reserva(self, numero_reserva):
         for tiquete in self.tiquetes:
-            if tiquete.getNumeroReserva() == numeroReserva:
+            if tiquete.get_numero_reserva() == numero_reserva:
                 return tiquete
         return None
 
-    def cancelarTiquete(self, tiquete):
+    def cancelar_tiquete(self, tiquete):
         for _tiquete in self.tiquetes:
             if _tiquete == tiquete:
-                _tiquete.liberarAsiento()
+                _tiquete.liberar_asiento()
                 self.tiquetes.remove(_tiquete)
                 break
 
-    def agregarTiquete(self, tiquete):
+    def agregar_tiquete(self, tiquete):
         self.tiquetes.append(tiquete)
 
-    def getTiquetes(self):
+    def get_tiquetes(self):
         return self.tiquetes
 
-    def setTiquetes(self, tiquetes):
+    def set_tiquetes(self, tiquetes):
         self.tiquetes = tiquetes
 
     @staticmethod
-    def getPasajeros():
+    def get_pasajeros():
         return Pasajero.pasajeros
 
     @staticmethod
-    def setPasajeros(pasajeros):
+    def set_pasajeros(pasajeros):
         Pasajero.pasajeros = pasajeros
 
     @staticmethod
-    def eliminarPasajeroPorNombre(nombre, idPasajero):
-        pasajero = Pasajero.buscarPasajeroPorNombre(nombre, idPasajero)
+    def eliminar_pasajero_por_nombre(nombre, id_pasajero):
+        pasajero = Pasajero.buscar_pasajero_por_nombre(nombre, id_pasajero)
         if pasajero:
             Pasajero.pasajeros.remove(pasajero)
 
     @staticmethod
-    def eliminarPasajeroPorId(idPasajero):
-        pasajero = Pasajero.buscarPasajero(idPasajero)
+    def eliminar_pasajero_por_id(id_pasajero):
+        pasajero = Pasajero.buscar_pasajero(id_pasajero)
         if pasajero:
             Pasajero.pasajeros.remove(pasajero)
 
     def __str__(self):
-        return f"     {self.getNombre()}        {self.getId()}        {self.getTelefono()}           {self.getCorreo()}     "
+        return f"     {self.get_nombre()}        {self.get_id()}        {self.get_telefono()}           {self.get_correo()}     "
