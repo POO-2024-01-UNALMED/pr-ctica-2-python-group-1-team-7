8abad81704db6a3ef9bad41e8997121ea5996947
samuelhd04 <shernandezdu@unal.edu.co>
2024-09-01 15:52:39 -0500

    Funcionalidad 2 y 3

--- src/uiMain/funcionalidad_2.py
+++ src/uiMain/funcionalidad_2.py
@@ -1,2 +1,174 @@
+import re
+
+def input():
+    # This is a placeholder for the input function.
+    # You may replace it with your custom implementation or use the built-in input() in actual usage.
+    return ""
+
 def reservar_tiquete():
-    pass
+    origen = input("Ingrese el origen: ").strip()
+    destino = input("Ingrese el destino: ").strip()
+
+    viajes = Empresa.buscar_viajes_origen_destino(origen.upper(), destino.upper())
+
+    if not viajes:
+        print("No se encontraron viajes disponibles para reservar\n")
+    else:
+        print(f"Estos son los viajes disponibles para la ruta {origen.upper()} --> {destino.upper()}:")
+        
+        for viaje in viajes:
+            print("-" * 92)
+            print("    FECHA          ORIGEN          DESTINO         HORA DE SALIDA     ID      PLACA BUS")
+            print("-" * 92)
+            print(viaje)
+            print()
+
+        id = input("Ingrese el id del viaje: ").strip()
+
+        viaje = Viaje.buscar_viaje(viajes, id)
+
+        if not viaje:
+            print(f"No se encontró ningún viaje con número de id {id} disponible para reservar\n")
+        else:
+            print("Asientos disponibles:\n")
+
+            tipos_asiento_fila = viaje.tipos_asiento()
+            fila = 1
+            indice = 0
+
+            print(TipoAsiento.PREFERENCIAL)
+            print("	       --")
+
+            for asiento in viaje.lista_asientos():
+                if len(asiento.get_numero()) == 2:
+                    if asiento.is_reservado():
+                        if "D" in asiento.get_numero():
+                            if fila == tipos_asiento_fila[indice]:
+                                print(f"{asiento.get_numero()}   |")
+                                print("	       --")
+                                if indice == 0:
+                                    print(TipoAsiento.PREMIUM)
+                                else:
+                                    print(TipoAsiento.ESTANDAR)
+                                print("	       --")
+                                if indice < len(tipos_asiento_fila) - 1:
+                                    indice += 1
+                            else:
+                                print("   |")
+                            fila += 1
+                        else:
+                            print("    ", end="")
+                    else:
+                        if "D" in asiento.get_numero():
+                            if fila == tipos_asiento_fila[indice]:
+                                print(f"{asiento.get_numero()}   |")
+                                print("	       --")
+                                if indice == 0:
+                                    print(TipoAsiento.PREMIUM)
+                                else:
+                                    print(TipoAsiento.ESTANDAR)
+                                print("	       --")
+                                if indice < len(tipos_asiento_fila) - 1:
+                                    indice += 1
+                            else:
+                                print(f"{asiento.get_numero()}   |")
+                                print()
+                            fila += 1
+                        else:
+                            print(f"{asiento.get_numero()}  ", end="")
+                else:
+                    if asiento.is_reservado():
+                        if "D" in asiento.get_numero():
+                            if fila == tipos_asiento_fila[indice]:
+                                print("   |")
+                                print("	       --")
+                                if indice == 0:
+                                    print(TipoAsiento.PREMIUM)
+                                else:
+                                    print(TipoAsiento.ESTANDAR)
+                                print("	       --")
+                                if indice < len(tipos_asiento_fila) - 1:
+                                    indice += 1
+                            else:
+                                print("   |")
+                                print()
+                            fila += 1
+                        else:
+                            print("    ", end="")
+                    else:
+                        if "D" in asiento.get_numero():
+                            if fila == tipos_asiento_fila[indice]:
+                                print(f"{asiento.get_numero()}  |")
+                                print("	       --")
+                                if indice == 0:
+                                    print(TipoAsiento.PREMIUM)
+                                else:
+                                    print(TipoAsiento.ESTANDAR)
+                                print("	       --")
+                                if indice < len(tipos_asiento_fila) - 1:
+                                    indice += 1
+                            else:
+                                print(f"{asiento.get_numero()}  |")
+                                print()
+                            fila += 1
+                        else:
+                            print(f"{asiento.get_numero()} ", end="")
+
+            print("	       --\n")
+
+            numero_asiento = input("Ingrese el número del asiento: ").strip()
+
+            while True:
+                asiento = viaje.buscar_asiento(numero_asiento)
+                if asiento is None or asiento.is_reservado():
+                    print("\nEl asiento no está disponible\n")
+                    numero_asiento = input("Ingrese otro número de asiento: ").strip()
+                else:
+                    break
+
+            viaje.reservar_asiento(numero_asiento, None)
+
+            print("Ingrese sus datos:\n")
+
+            nombre = input("Nombre completo: ").strip()
+            id_pasajero = input("Número de identificación (6 dígitos): ").strip()
+
+            while len(id_pasajero) != 6:
+                id_pasajero = input("Número de identificación (6 dígitos): ").strip()
+
+            pasajero = Pasajero.buscar_pasajero(id_pasajero)
+
+            if not pasajero:
+                telefono = input("Teléfono: ").strip()
+                while not re.match(r'^\d{10}$', telefono):
+                    print("Teléfono inválido\nEl teléfono debe contener 10 dígitos\n")
+                    telefono = input("Teléfono: ").strip()
+
+                correo = input("Correo electrónico: ").strip()
+                while not re.match(r'^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$', correo):
+                    print("Correo inválido\nEl correo debe tener la estructura (abcde@xyz.com)\n")
+                    correo = input("Correo electrónico: ").strip()
+
+                pasajero = Pasajero(nombre, id_pasajero, telefono, correo)
+
+            asiento = viaje.buscar_asiento(numero_asiento)
+            tiquete = Tiquete(pasajero, viaje, asiento)
+            pasajero.agregar_tiquete(tiquete)
+
+            print("Tiquete reservado exitosamente\n")
+            print("-" * 34)
+            print(f"    Tiquete No. {tiquete.get_numero_reserva()}")
+            print("-" * 34)
+            print(f"Nombre del pasajero: {nombre}")
+            print(f"Id del pasajero: {id_pasajero}")
+            print(f"Teléfono: {pasajero.get_telefono()}")
+            print(f"Correo: {pasajero.get_correo()}")
+            print(f"Asiento: {asiento}")
+            print(f"Empresa: {viaje.get_empresa().get_nombre()}")
+            print(f"Placa del bus: {viaje.get_bus().get_placa()}")
+            print(f"Id del viaje: {viaje.get_id()}")
+            print(f"Fecha y hora: {viaje.get_fecha()} {viaje.get_hora()}")
+            print(f"Origen: {viaje.get_terminal_origen().get_ubicacion()}")
+            print(f"Destino: {viaje.get_terminal_destino().get_ubicacion()}")
+            print("-" * 34)
+            print()
--- src/uiMain/funcionalidad_3.py
+++ src/uiMain/funcionalidad_3.py
@@ -1,2 +1,321 @@
 def gestionar_tiquetes():
-    pass
+    id_pasajero = input("Ingrese el número de identificación del pasajero: ")
+
+    while len(id_pasajero) != 6:
+        print("Número de identificación (6 dígitos): ")
+        id_pasajero = input()
+
+    print()
+
+    pasajero = Pasajero.buscar_pasajero(id_pasajero)
+
+    if pasajero is None or not pasajero.get_tiquetes():
+        print("No hay tiquetes asociados con el número de identificación\n")
+    else:
+        tiquetes_validos = pasajero.buscar_tiquetes("validos")
+        tiquetes_vencidos = pasajero.buscar_tiquetes("vencidos")
+
+        if tiquetes_validos:
+            print("Tiquetes válidos")
+            print("-" * 93)
+            print("    NUMERO DE RESERVA     NOMBRE        ASIENTO             FECHA DEL VIAJE      ID VIAJE     ")
+            print("-" * 93)
+
+            for tiquete in tiquetes_validos:
+                print(tiquete)
+
+            print()
+
+        if tiquetes_vencidos:
+            print("Tiquetes vencidos")
+            print("-" * 93)
+            print("    NUMERO DE RESERVA     NOMBRE        ASIENTO             FECHA DEL VIAJE      ID VIAJE     ")
+            print("-" * 93)
+
+            for tiquete in tiquetes_vencidos:
+                print(tiquete)
+
+            print()
+
+        respuesta1 = input("¿Desea escoger algún tiquete? (si/no): ").lower()
+        print()
+
+        if respuesta1 == "si":
+            numero_reserva = input("Escoja el tiquete ingresando el número de reserva: ")
+            print()
+
+            if pasajero.buscar_tiquete(numero_reserva) is None:
+                print(f"No se encontró ningún tiquete con el número de reserva {numero_reserva}\n")
+            else:
+                for tiquete in tiquetes_vencidos:
+                    if tiquete.get_numero_reserva() == numero_reserva:
+                        print("Detalles del viaje")
+                        print("-" * 92)
+                        print("    FECHA          ORIGEN          DESTINO         HORA DE SALIDA     ID      PLACA BUS")
+                        print("-" * 92)
+                        print(tiquete.get_viaje())
+                        print("\nDetalles del tiquete")
+                        print("-" * 93)
+                        print("    NUMERO DE RESERVA     NOMBRE        ASIENTO             FECHA DEL VIAJE      ID VIAJE     ")
+                        print("-" * 93)
+                        print(tiquete)
+                        print()
+                        break
+
+                for tiquete in tiquetes_validos:
+                    if tiquete.get_numero_reserva() == numero_reserva:
+                        print("¿Qué desea hacer?\n")
+                        print("1. Cancelarlo")
+                        print("2. Modificarlo")
+                        respuesta2 = input("Ingrese el número de la operación: ")
+                        print()
+
+                        if respuesta2 == "1":
+                            pasajero.cancelar_tiquete(tiquete)
+                            print("Tiquete cancelado exitosamente\n")
+                        else:
+                            print("¿Qué desea hacer?\n")
+                            print("1. Cambiar de asiento")
+                            print("2. Elegir otro viaje")
+                            respuesta3 = input("Ingrese el número de la operación: ")
+                            print()
+
+                            if respuesta3 == "1":
+                                viaje = tiquete.get_viaje()
+
+                                if not viaje.tiene_sillas():
+                                    print("No hay más asientos disponibles\n")
+                                else:
+                                    print("Asientos disponibles:\n")
+                                    tipos_asiento_fila = viaje.tipos_asiento()
+                                    fila = 1
+                                    indice = 0
+
+                                    print(TipoAsiento.PREFERENCIAL)
+                                    print("	       --")
+
+                                    for asiento in viaje.lista_asientos():
+                                        if len(asiento.get_numero()) == 2:
+                                            if asiento.is_reservado():
+                                                if "D" in asiento.get_numero():
+                                                    if fila == tipos_asiento_fila[indice]:
+                                                        print(f"{asiento.get_numero()}   |")
+                                                        print("	       --")
+
+                                                        if indice == 0:
+                                                            print(TipoAsiento.PREMIUM)
+                                                            print("	       --")
+                                                        else:
+                                                            print(TipoAsiento.ESTANDAR)
+                                                            print("	       --")
+
+                                                        if indice < len(tipos_asiento_fila) - 1:
+                                                            indice += 1
+                                                    else:
+                                                        print("  " + "   |")
+                                                    fila += 1
+                                                else:
+                                                    print("    ", end="")
+                                            else:
+                                                if "D" in asiento.get_numero():
+                                                    if fila == tipos_asiento_fila[indice]:
+                                                        print(f"{asiento.get_numero()}   |")
+                                                        print("	       --")
+
+                                                        if indice == 0:
+                                                            print(TipoAsiento.PREMIUM)
+                                                            print("	       --")
+                                                        else:
+                                                            print(TipoAsiento.ESTANDAR)
+                                                            print("	       --")
+
+                                                        if indice < len(tipos_asiento_fila) - 1:
+                                                            indice += 1
+                                                    else:
+                                                        print(f"{asiento.get_numero()}   |")
+                                                        print()
+                                                    fila += 1
+                                                else:
+                                                    print(f"{asiento.get_numero()}  ", end="")
+                                        else:
+                                            if asiento.is_reservado():
+                                                if "D" in asiento.get_numero():
+                                                    if fila == tipos_asiento_fila[indice]:
+                                                        print("   " + "  |")
+                                                        print("	       --")
+
+                                                        if indice == 0:
+                                                            print(TipoAsiento.PREMIUM)
+                                                            print("	       --")
+                                                        else:
+                                                            print(TipoAsiento.ESTANDAR)
+                                                            print("	       --")
+
+                                                        if indice < len(tipos_asiento_fila) - 1:
+                                                            indice += 1
+                                                    else:
+                                                        print("   " + "  |")
+                                                        print()
+                                                    fila += 1
+                                                else:
+                                                    print("    ", end="")
+                                            else:
+                                                if "D" in asiento.get_numero():
+                                                    if fila == tipos_asiento_fila[indice]:
+                                                        print(f"{asiento.get_numero()}  |")
+                                                        print("	       --")
+
+                                                        if indice == 0:
+                                                            print(TipoAsiento.PREMIUM)
+                                                            print("	       --")
+                                                        else:
+                                                            print(TipoAsiento.ESTANDAR)
+                                                            print("	       --")
+
+                                                        if indice < len(tipos_asiento_fila) - 1:
+                                                            indice += 1
+                                                    else:
+                                                        print(f"{asiento.get_numero()}  |")
+                                                        print()
+                                                    fila += 1
+                                                else:
+                                                    print(f"{asiento.get_numero()} ", end="")
+
+                                    print("	       --\n")
+                                    numero_asiento = input("Ingrese el número del asiento: ")
+
+                                    while viaje.buscar_asiento(numero_asiento) is None or viaje.buscar_asiento(numero_asiento).is_reservado():
+                                        print("\nEl asiento no está disponible\n")
+                                        numero_asiento = input("Ingrese otro número de asiento: ")
+
+                                    tiquete.cambiar_asiento(viaje.buscar_asiento(numero_asiento))
+
+                            elif respuesta3 == "2":
+                                origen = input("Ingrese el origen: ")
+                                print()
+                                destino = input("Ingrese el destino: ")
+                                print()
+
+                                print(f"Estos son los viajes disponibles para la ruta {origen.upper()} --> {destino.upper()}:")
+                                print("-" * 92)
+                                print("    FECHA          ORIGEN          DESTINO         HORA DE SALIDA     ID      PLACA BUS")
+                                print("-" * 92)
+
+                                for viaje in Empresa.buscar_viajes(origen.upper(), destino.upper()):
+                                    print(viaje)
+
+                                print()
+                                id_viaje = input("Ingrese el id del viaje: ")
+                                print()
+
+                                viaje = Empresa.buscar_viaje(id_viaje)
+
+                                if not viaje.lista_asientos():
+                                    print("No hay más asientos disponibles")
+                                    break
+
+                                print("Asientos disponibles:\n")
+                                tipos_asiento_fila = viaje.tipos_asiento()
+                                fila = 1
+                                indice = 0
+
+                                print(TipoAsiento.PREFERENCIAL)
+                                print("	       --")
+
+                                for asiento in viaje.lista_asientos():
+                                    if len(asiento.get_numero()) == 2:
+                                        if asiento.is_reservado():
+                                            if "D" in asiento.get_numero():
+                                                if fila == tipos_asiento_fila[indice]:
+                                                    print(f"{asiento.get_numero()}   |")
+                                                    print("	       --")
+
+                                                    if indice == 0:
+                                                        print(TipoAsiento.PREMIUM)
+                                                        print("	       --")
+                                                    else:
+                                                        print(TipoAsiento.ESTANDAR)
+                                                        print("	       --")
+
+                                                    if indice < len(tipos_asiento_fila) - 1:
+                                                        indice += 1
+                                                else:
+                                                    print("  " + "   |")
+                                                fila += 1
+                                            else:
+                                                print("    ", end="")
+                                        else:
+                                            if "D" in asiento.get_numero():
+                                                if fila == tipos_asiento_fila[indice]:
+                                                    print(f"{asiento.get_numero()}   |")
+                                                    print("	       --")
+
+                                                    if indice == 0:
+                                                        print(TipoAsiento.PREMIUM)
+                                                        print("	       --")
+                                                    else:
+                                                        print(TipoAsiento.ESTANDAR)
+                                                        print("	       --")
+
+                                                    if indice < len(tipos_asiento_fila) - 1:
+                                                        indice += 1
+                                                else:
+                                                    print(f"{asiento.get_numero()}   |")
+                                                    print()
+                                                fila += 1
+                                            else:
+                                                print(f"{asiento.get_numero()}  ", end="")
+                                    else:
+                                        if asiento.is_reservado():
+                                            if "D" in asiento.get_numero():
+                                                if fila == tipos_asiento_fila[indice]:
+                                                    print("   " + "  |")
+                                                    print("	       --")
+
+                                                    if indice == 0:
+                                                        print(TipoAsiento.PREMIUM)
+                                                        print("	       --")
+                                                    else:
+                                                        print(TipoAsiento.ESTANDAR)
+                                                        print("	       --")
+
+                                                    if indice < len(tipos_asiento_fila) - 1:
+                                                        indice += 1
+                                                else:
+                                                    print("   " + "  |")
+                                                    print()
+                                                fila += 1
+                                            else:
+                                                print("    ", end="")
+                                        else:
+                                            if "D" in asiento.get_numero():
+                                                if fila == tipos_asiento_fila[indice]:
+                                                    print(f"{asiento.get_numero()}  |")
+                                                    print("	       --")
+
+                                                    if indice == 0:
+                                                        print(TipoAsiento.PREMIUM)
+                                                        print("	       --")
+                                                    else:
+                                                        print(TipoAsiento.ESTANDAR)
+                                                        print("	       --")
+
+                                                    if indice < len(tipos_asiento_fila) - 1:
+                                                        indice += 1
+                                                else:
+                                                    print(f"{asiento.get_numero()}  |")
+                                                    print()
+                                                fila += 1
+                                            else:
+                                                print(f"{asiento.get_numero()} ", end="")
+
+                                print("	       --\n")
+                                numero_asiento = input("Ingrese el número del asiento: ")
+
+                                while viaje.buscar_asiento(numero_asiento) is None or viaje.buscar_asiento(numero_asiento).is_reservado():
+                                    print("\nEl asiento no está disponible\n")
+                                    numero_asiento = input("Ingrese otro número de asiento: ")
+
+                                tiquete.cambiar_viaje(viaje, viaje.buscar_asiento(numero_asiento))
+                                pasajero.cancelar_tiquete(tiquete)
+
